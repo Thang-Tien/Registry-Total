f58876fda4e30d6901b4fc2088933624940cbd44
dzgod1905 <ngocvu7878@gmail.com>
2023-11-14 03:04:19 +0700

    feat: Admin navbar done.

---
+++ apps/next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+  transpilePackages: ["protocol"],
+};
