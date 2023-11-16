5ea8db39eab54049d4478131c94ef8da35f068ff
dzgod1905 <ngocvu7878@gmail.com>
2023-11-17 05:44:26 +0700

    feat: Homepage almost done.

---
+++ apps/containers/Admin/HomePage/containers/ChartPie.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { Card } from "antd";
+import { Column, Line, Pie } from "@ant-design/plots";
+import React from "react";
+
+const ChartPie: React.FC = () => {
+  const data = [
+    {
+      type: "Đã hết hạn",
+      value: 6226,
+    },
+    {
+      type: "Sắp hết hạn",
+      value: 230,
+    },
+    {
+      type: "Mới (dự đoán)",
+      value: 237,
+    },
+  ];
+  return (
+    <div style={{ width: "calc((100vw - 256px - 64px - 100px) /3 * 2)" }}>
+      <Card
+        title="Trong tháng này"
+        style={{ width: "calc((100vw - 256px - 64px -100px)/3)" }}
+      >
+        <Pie
+          appendPadding={10}
+          data={data}
+          angleField="value"
+          colorField="type"
+          radius={0.75}
+          label={{
+            type: "inner",
+            content: "{value}",
+          }}
+          interactions={[
+            {
+              type: "element-selected",
+            },
+            {
+              type: "element-active",
+            },
+          ]}
+          height={250}
+        />
+      </Card>
+    </div>
+  );
+};
+
+export default ChartPie;
--- apps/containers/Admin/HomePage/index.tsx
+++ apps/containers/Admin/HomePage/index.tsx
@@ -2,6 +2,12 @@ import cx from "classnames";
 import styles from "./index.module.scss";
 import Flex from "@/modules/ui/components/Flex";
 import NavBar from "../NavBar";
+import All from "./containers/All";
+import ThisYear from "./containers/ThisYear";
+import ThisMonth from "./containers/ThisMonth";
+import Centers from "./containers/Centers";
+import ChartLine from "./containers/ChartLine";
+import ChartPie from "./containers/ChartPie";
 
 export type Props = {
   className?: string;
@@ -9,9 +15,26 @@ export type Props = {
 };
 
 export default function HomePage({ className, style }: Props) {
-  return <div className={cx(className, styles.container)} style={style}>
-    <Flex.Row>
-      <NavBar active="centers" />
-    </Flex.Row>
-  </div>;
+  return (
+    <div className={cx(className, styles.container)} style={style}>
+      <Flex.Row style={{height: "100vh", width: "100vw"}} padding={"0"}>
+        <NavBar active="centers" />"
+        <div style={{background: "#e4e4e7",width:"calc(100vw - 256px - 8px)"}}>
+        <Flex.Col gap={"20px"} style={{padding: "24px 32px"}}>
+          <Flex.Row gap={"20px"}>
+            <All />
+            <ThisYear />
+            <ThisMonth />
+            <Centers />
+          </Flex.Row>
+          <Flex.Row gap={"20px"}>
+            <ChartLine />
+            <ChartPie />
+          </Flex.Row>
+        </Flex.Col>
+        </div>
+        
+      </Flex.Row>
+    </div>
+  );
 }
