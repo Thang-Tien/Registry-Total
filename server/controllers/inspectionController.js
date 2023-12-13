8ea81ed823b8f211abeae3063e1826af66e40413
VuThiThanhVinh-21020422 <21020422@vnu.edu.vn>
2023-12-13 20:57:57 +0700

    add count expire this month api

--- server/controllers/inspectionController.js
+++ server/controllers/inspectionController.js
@@ -1,154 +1,223 @@
-const connection = require('../config/DBConnection')
-const utils = require('../utils/utils')
-const PDFDocument = require('pdfkit');
-const fs = require('fs');
-// đếm tổng số lượng đăng kiểm của centre mà staff đang làm việc 
+const connection = require("../config/DBConnection");
+const utils = require("../utils/utils");
+const PDFDocument = require("pdfkit");
+const fs = require("fs");
+// DONE
+// đếm tổng số lượng đăng kiểm của centre mà staff đang làm việc
 exports.countInspectionsOfEachCentre = async (req, res) => {
-    const centreId = req.params.centreId; // Extract centreId from the URL
+	const centreId = req.params.centreId; // Extract centreId from the URL
 
-    let queryString = utils.generateQueryStringWithDate(req.query, 'inspection_date');
-    connection.query(`SELECT COUNT(*) as total 
+	let queryString = utils.generateQueryStringWithDate(
+		req.query,
+		"inspection_date"
+	);
+	connection.query(
+		`SELECT COUNT(*) as total 
                      FROM inspections i 
                      INNER JOIN registration_centres r 
                      ON i.centre_id = r.centre_id 
-                     WHERE ${queryString ? queryString : 1} AND i.centre_id = ?`, 
-    [centreId], (err, result, fields) => {
-        if (err) {
-            return res.status(500).json({
-                status: 'Failed',
-                error: err
-            });
-        } else {
-            return res.status(200).json({
-                status: "Success",
-                data: result
-            });
-        }
-    });
+                     WHERE ${
+							queryString ? queryString : 1
+						} AND i.centre_id = ?`,
+		[centreId],
+		(err, result, fields) => {
+			if (err) {
+				return res.status(500).json({
+					status: "Failed",
+					error: err,
+				});
+			} else {
+				return res.status(200).json({
+					status: "Success",
+					data: result,
+				});
+			}
+		}
+	);
 };
 
-
-// đếm tổng số lượng đăng kiểm hết hạn của trung tâm mà staff đang làm việc 
+// DONE nhưng không liên quan
+// đếm tổng số lượng đăng kiểm đã hết hạn của trung tâm mà staff đang làm việc
 exports.countTotalExpiredInspectionsOfEachCentre = async (req, res) => {
-    const centreId = req.params.centreId;
-    connection.query(`SELECT COUNT(*) 
+	const centreId = req.params.centreId;
+	connection.query(
+		`SELECT COUNT(*) 
                     FROM inspections 
-                    WHERE expired_date < NOW() AND centre_id = ?`,[centreId], (err, result, fields) => {
-        if (err) {
-            return res.status(500).json({
-                status: 'Failed',
-                error: {
-                    err
-                }
-            })
-        } else {
-            return res.status(200).json({
-                status: "Success",
-                data: result
-            })
-        }
-    })
-}
+                    WHERE expired_date < NOW() AND centre_id = ?`,
+		[centreId],
+		(err, result, fields) => {
+			if (err) {
+				return res.status(500).json({
+					status: "Failed",
+					error: {
+						err,
+					},
+				});
+			} else {
+				return res.status(200).json({
+					status: "Success",
+					data: result,
+				});
+			}
+		}
+	);
+};
 
-// đếm tổng số lượng đăng kiểm sắp hết hạn của các trung tâm staff đang làm việc 
+// DONE
+// đếm tổng số lượng đăng kiểm đã hết hạn của trung tâm mà staff đang làm việc trong tháng này
+exports.countTotalExpiredInspectionsOfEachCentreThisMonth = async (
+	req,
+	res
+) => {
+	const centreId = req.params.centreId;
+	// Lấy ngày đầu tiên của tháng hiện tại
+	const firstDayOfMonth = new Date(
+		new Date().getFullYear(),
+		new Date().getMonth(),
+		1
+	);
+	connection.query(
+		`SELECT COUNT(*) 
+                    FROM inspections 
+                    WHERE expired_date < NOW() AND centre_id = ? AND expired_date >= ?`,
+		[centreId, firstDayOfMonth],
+		(err, result, fields) => {
+			if (err) {
+				return res.status(500).json({
+					status: "Failed",
+					error: {
+						err,
+					},
+				});
+			} else {
+				return res.status(200).json({
+					status: "Success",
+					data: result,
+				});
+			}
+		}
+	);
+};
+
+// DONE
+// đếm tổng số lượng đăng kiểm sắp hết hạn của các trung tâm staff đang làm việc
 exports.countTotalAboutToExpiredInspectionsOfEachCentre = async (req, res) => {
-    const centreId = req.params.centreId;
-    connection.query(`SELECT COUNT(*) 
+	const centreId = req.params.centreId;
+	connection.query(
+		`SELECT COUNT(*) 
                     FROM inspections 
                     WHERE expired_date >= DATE_SUB(NOW(), INTERVAL 1 MONTH) 
-                            AND expired_date <= NOW() AND centre_id = ?`,[centreId] ,
-    (err, result, fields) => {
-        if (err) {
-            return res.status(500).json({
-                status: 'Failed',
-                error: {
-                    err
-                }
-            })
-        } else {
-            return res.status(200).json({
-                status: "Success",
-                data: result
-            })
-        }
-    })
-}
+                            AND expired_date <= NOW() AND centre_id = ?`,
+		[centreId],
+		(err, result, fields) => {
+			if (err) {
+				return res.status(500).json({
+					status: "Failed",
+					error: {
+						err,
+					},
+				});
+			} else {
+				return res.status(200).json({
+					status: "Success",
+					data: result,
+				});
+			}
+		}
+	);
+};
 
-// dự đoán số lượng xe đăng kiểm mới của mỗi trung tâm 
+// NOT DONE
+// dự đoán số lượng xe đăng kiểm mới của mỗi trung tâm
 // (xe đăng ký dự đoán ở tỉnh nào thì dự đoán số lượng xe đăng kiểm mới của trung tâm ở tỉnh đó)
 exports.predictAboutToInspectOfEachCentre = async (req, res) => {
-    const centreId = req.params.centreId;
-    connection.query(`SELECT COUNT(*) 
+	const centreId = req.params.centreId;
+	connection.query(
+		`SELECT COUNT(*) 
                     FROM cars c INNER JOIN registration_centres r
                     ON c.predictedAddress = r.address 
-                    WHERE c.inspected = false AND r.centre_id = ?`, [centreId], (err, result, fields) => {
-        if (err) {
-            return res.status(500).json({
-                status: "Failed",
-                message: err
-            })
-        } else {
-            return res.status(202).json({
-                status: "Success",
-                data: result
-            })
-        }
-    })
-}
+                    WHERE c.inspected = false AND r.centre_id = ?`,
+		[centreId],
+		(err, result, fields) => {
+			if (err) {
+				return res.status(500).json({
+					status: "Failed",
+					message: err,
+				});
+			} else {
+				return res.status(202).json({
+					status: "Success",
+					data: result,
+				});
+			}
+		}
+	);
+};
 
-// đưa ra thông tin đăng kiểm của trung tâm mà staff đang làm việc 
-exports.getInspection = (req, res) => {
-    const centreId = req.params.centreId;
-    let queryString = utils.generateQueryString(req.query)
-    connection.query(`SELECT * FROM inspections 
-                    WHERE ${queryString ? queryString : 1} AND centre_id = ?`,[centreId], 
-     (err, result, fields) => {
-        if (err) {
-            return res.status(500).json({
-                status: "Failed",
-                message: err
-            })
-        } else if (result.length == 0) {
-            return res.status(404).json({
-                status: "Failed",
-                message: `Can't find inspection with ${utils.generateErrorQueryValue(req.query)}`
-            })
-        } else {
-            return res.status(200).json({
-                status: "Success",
-                data: result
-            })
-        }
-    })
-}
-
-
+// DONE
 // đưa ra thông tin đăng kiểm của trung tâm mà staff đang làm việc
+exports.getInspection = (req, res) => {
+	const centreId = req.params.centreId;
+	let queryString = utils.generateQueryString(req.query);
+	connection.query(
+		`SELECT * FROM inspections 
+                    WHERE ${queryString ? queryString : 1} AND centre_id = ?`,
+		[centreId],
+		(err, result, fields) => {
+			if (err) {
+				return res.status(500).json({
+					status: "Failed",
+					message: err,
+				});
+			} else if (result.length == 0) {
+				return res.status(404).json({
+					status: "Failed",
+					message: `Can't find inspection with ${utils.generateErrorQueryValue(
+						req.query
+					)}`,
+				});
+			} else {
+				return res.status(200).json({
+					status: "Success",
+					data: result,
+				});
+			}
+		}
+	);
+};
+
+// DONE
+// đưa ra thông tin đăng kiểm của trung tâm mà staff đang làm việc bao gồm thông tin chi tiết
 // tất cả thông tin chi tiết ( nối ba bảng gồm: inspections, cars, car_owners)
-exports.getStaffInspection = (req, res) => {
-    const centreId = req.params.centreId;
-    let queryString = utils.generateQueryString(req.query)
-    connection.query(`SELECT * 
+exports.getInspectionAndOwner = (req, res) => {
+	const centreId = req.params.centreId;
+	let queryString = utils.generateQueryString(req.query);
+	connection.query(
+		`SELECT * 
                     FROM inspections i 
                     INNER JOIN cars c ON c.car_id = i.car_id
                     INNER JOIN car_owners co ON co.owner_id = c.owner_id
-                    WHERE ${queryString ? queryString : 1} AND i.centre_id = ?`, [centreId],
-     (err, result, fields) => {
-        if (err) {
-            return res.status(500).json({
-                status: "Failed",
-                message: err
-            })
-        } else if (result.length == 0) {
-            return res.status(404).json({
-                status: "Failed",
-                message: `Can't find inspection with ${utils.generateErrorQueryValue(req.query)}`
-            })
-        } else {
-            return res.status(200).json({
-                status: "Success",
-                data: result
-            })
-        }
-    })
-}
+                    WHERE ${queryString ? queryString : 1} AND i.centre_id = ?`,
+		[centreId],
+		(err, result, fields) => {
+			if (err) {
+				return res.status(500).json({
+					status: "Failed",
+					message: err,
+				});
+			} else if (result.length == 0) {
+				return res.status(404).json({
+					status: "Failed",
+					message: `Can't find inspection with ${utils.generateErrorQueryValue(
+						req.query
+					)}`,
+				});
+			} else {
+				return res.status(200).json({
+					status: "Success",
+					data: result,
+				});
+			}
+		}
+	);
+};
--- server/routes/inspectionRoute.js
+++ server/routes/inspectionRoute.js
@@ -1,15 +1,31 @@
-const express = require('express')
-const router = express.Router({mergeParams: true})
-const inspectionController = require('../controllers/inspectionController')
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const inspectionController = require("../controllers/inspectionController");
 
-router.get('/stat/each_centre/count/:centreId', inspectionController.countInspectionsOfEachCentre)
-router.get('/stat/each_centre/count/expired/:centreId', inspectionController.countTotalExpiredInspectionsOfEachCentre)
-router.get('/stat/each_centre/count/about_to_expired/:centreId', inspectionController.countTotalAboutToExpiredInspectionsOfEachCentre)
-router.get('/stat/all_centre/prediction/about_to_inspect_of_each_centre/:centreId', inspectionController.predictAboutToInspectOfEachCentre)
-router.get('/:centreId', inspectionController.getInspection)
-router.get('/get_staff_inspection/:centreId', inspectionController.getStaffInspection)
+router.get(
+	"/stat/each_centre/count/:centreId",
+	inspectionController.countInspectionsOfEachCentre
+);
+router.get(
+	"/stat/each_centre/count/expired/:centreId",
+	inspectionController.countTotalExpiredInspectionsOfEachCentre
+);
+router.get(
+	"/stat/each_centre/count/expired/month/:centreId",
+	inspectionController.countTotalExpiredInspectionsOfEachCentreThisMonth
+);
+router.get(
+	"/stat/each_centre/count/about_to_expired/:centreId",
+	inspectionController.countTotalAboutToExpiredInspectionsOfEachCentre
+);
+router.get(
+	"/stat/all_centre/prediction/about_to_inspect_of_each_centre/:centreId",
+	inspectionController.predictAboutToInspectOfEachCentre
+);
+router.get("/:centreId", inspectionController.getInspection);
+router.get(
+	"/get_inspection_owner/:centreId",
+	inspectionController.getInspectionAndOwner
+);
 
-
-
-module.exports = router
-
+module.exports = router;
