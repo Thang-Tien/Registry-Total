2b6f4bb4e11654a162197083d13af5f10013d86c
Thang Tien_21020403 <cao.hl.thang@gmail.com>
2023-12-13 22:02:26 +0700

    configure db, add db

--- server/config/DBConnection.js
+++ server/config/DBConnection.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'registry_total'
+    host: '10.110.77.167',
+    user: 'fall2324w3g10',
+    database: 'fall2324w3g10_dbtest'
 })
 
 connection.connect(function(err) {
