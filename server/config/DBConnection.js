58180bcce4e01bff76f469e4c913bc19448579c1
Thang Tien_21020403 <cao.hl.thang@gmail.com>
2023-12-13 23:35:00 +0700

    Update DBConnection.js

--- server/config/DBConnection.js
+++ server/config/DBConnection.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
-    // host: '10.110.77.167',
-    // user: 'fall2324w3g10',
-    // database: 'fall2324w3g10_dbtest',
-    // password: 'qwertyuiop'
+    host: '10.110.77.167',
+    user: 'fall2324w3g10',
+    database: 'fall2324w3g10_dbtest',
+    password: 'qwertyuiop'
 
-    host: 'localhost',
-    user: 'root',
-    database: 'registry_total',
+    // host: 'localhost',
+    // user: 'root',
+    // database: 'registry_total',
 })
 
 connection.connect(function(err) {
