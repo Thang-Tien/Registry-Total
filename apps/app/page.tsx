65b60fed589567c222915565503bc009f8ee88a7
thanhthien1305 <100114914+thanhthien1305@users.noreply.github.com>
2023-12-16 02:39:28 +0700

    done css authentication

--- apps/app/page.tsx
+++ apps/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import AuthenticationPage from "../containers/Admin/Authentication";
 import HomePage from "../containers/Admin/HomePage";
+import "modules/ui/styles/page.css"
+
 
 export default function RouteToHomePage() {
   return <AuthenticationPage/>
