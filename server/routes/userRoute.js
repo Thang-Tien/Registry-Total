831b9df1ffff8750d88a76c5cb2ece20b34835d6
Thang Tien_21020403 <cao.hl.thang@gmail.com>
2023-12-15 20:38:09 +0700

    add forgot password function

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -5,5 +5,6 @@ const router = express.Router()
 
 router.post('/login', authController.login)
 router.post('/change-password', authController.authenticateToken, authController.changePassword)
-
+router.post('/forgot-password', authController.handleForgotPassword)
+router.post('/reset-password/:token', authController.resetPassword)
 module.exports = router
