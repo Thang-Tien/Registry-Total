7fe6612eeac216225dd81c23d3b38d9bfbf0f450
VuThiThanhVinh-21020422 <21020422@vnu.edu.vn>
2023-12-25 02:36:20 +0700

    add search number plate and inspection num

--- server/routes/searchRoute.js
+++ server/routes/searchRoute.js
@@ -1,3 +1,12 @@
-// const express = require("express");
-// const router = express.Router({ mergeParams: true });
-// const searchController = require("../controllers/searchController");
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const searchController = require("../controllers/searchController");
+router.get(
+	"/inspection_number/:inspection_number",
+	searchController.getInspectionByInspectionNumber
+);
+router.get(
+	"/number_plate/:number_plate",
+	searchController.getInspectionByNumberPlate
+);
+module.exports = router;
