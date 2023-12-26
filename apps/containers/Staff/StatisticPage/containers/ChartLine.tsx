19b75007773c4373673dc52f933b3fa0db211670
Nguyen Dang Quang <quangnguyenvk@gmail.com>
2023-12-27 04:15:34 +0700

    Add statistic page w\ sample data, few changes

---
+++ apps/containers/Staff/StatisticPage/containers/ChartLine.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { Card } from "antd";
+import { Column, Line } from "@ant-design/plots";
+import React from "react";
+
+const ChartLine: React.FC = () => {
+    const data = [
+        {
+            month: "1/2023",
+            value: 25,
+        },
+        {
+            month: "2/2023",
+            value: 12,
+        },
+        {
+            month: "3/2023",
+            value: 9,
+        },
+        {
+            month: "4/2023",
+            value: 15,
+        },
+        {
+            month: "5/2023",
+            value: 8,
+        },
+        {
+            month: "6/2023",
+            value: 21,
+        },
+        {
+            month: "7/2023",
+            value: 7,
+        },
+        {
+            month: "8/2023",
+            value: 17,
+        },
+        {
+            month: "9/2023",
+            value: 13,
+        },
+    ];
+
+    return (
+        <div style={{ width: "calc((100vw - 256px - 64px - 20px) /3 *2)" }}>
+            <Card
+                title="Biểu đồ đăng kiểm"
+                style={{
+                    width: "calc((100vw - 256px - 64px - 20px) /3 *2)",
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                }}
+                bodyStyle={{ flex: "1" }}
+            >
+                <Line
+                    data={data}
+                    xField="month"
+                    yField="value"
+                    xAxis={{ tickCount: 5 }}
+                    smooth={true}
+                    meta={{
+                        value: {
+                            alias: "Số lượng",
+                        },
+                        month: {
+                            alias: "Tháng",
+                        },
+                    }}
+                    slider={{ start: 0, end: 1 }}
+                    style={{ height: "calc(100% - 8px)" }}
+                />
+            </Card>
+        </div>
+    );
+};
+
+export default ChartLine;
