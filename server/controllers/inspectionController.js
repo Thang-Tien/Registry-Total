3a1b9b001d678d4f0213704a9f755bd190eff2ee
VuThiThanhVinh-21020422 <21020422@vnu.edu.vn>
2023-12-27 22:26:18 +0700

    update all inspection functions

--- server/controllers/inspectionController.js
+++ server/controllers/inspectionController.js
@@ -2,13 +2,9 @@ const connection = require("../config/DBConnection");
 const utils = require("../utils/utils");
 const PDFDocument = require("pdfkit");
 const fs = require("fs");
-const { user_id, centre_id, login } = require("./authController");
-
-// DONE
+//DONE
 // đếm tổng số lượng đăng kiểm của centre mà staff đang làm việc
 exports.countInspectionsOfEachCentre = async (req, res) => {
-	const centreId = req.params.centreId; // Extract centreId from the URL
-
 	let queryString = utils.generateQueryStringWithDate(
 		req.query,
 		"inspection_date"
@@ -21,7 +17,7 @@ exports.countInspectionsOfEachCentre = async (req, res) => {
                      WHERE ${
 							queryString ? queryString : 1
 						} AND i.centre_id = ?`,
-		[centreId],
+		[req.user.centre_id],
 		(err, result, fields) => {
 			if (err) {
 				return res.status(500).json({
@@ -37,9 +33,9 @@ exports.countInspectionsOfEachCentre = async (req, res) => {
 		}
 	);
 };
+//DONE
 // đếm tổng số lượng đăng kiểm của centre mà staff đang làm việc trong tháng này
 exports.countInspectionsOfEachCentreThisMonth = async (req, res) => {
-	const centreId = req.params.centreId; // Extract centreId from the URL
 	const firstDayOfMonth = new Date(
 		new Date().getFullYear(),
 		new Date().getMonth(),
@@ -57,7 +53,7 @@ exports.countInspectionsOfEachCentreThisMonth = async (req, res) => {
                      WHERE ${
 							queryString ? queryString : 1
 						} AND i.inspection_date <= LAST_DAY(NOW()) AND i.centre_id = ? AND i.inspection_date >= ?`,
-		[centreId, firstDayOfMonth],
+		[req.user.centre_id, firstDayOfMonth],
 		(err, result, fields) => {
 			if (err) {
 				return res.status(500).json({
@@ -73,9 +69,9 @@ exports.countInspectionsOfEachCentreThisMonth = async (req, res) => {
 		}
 	);
 };
+//DONE
 // đếm tổng số lượng đăng kiểm của centre mà staff đang làm việc trong năm này
 exports.countInspectionsOfEachCentreThisYear = async (req, res) => {
-	const centreId = req.params.centreId; // Extract centreId from the URL
 	const firstDayOfMonth = new Date(
 		new Date().getFullYear(),
 		new Date().getMonth(),
@@ -93,7 +89,7 @@ exports.countInspectionsOfEachCentreThisYear = async (req, res) => {
                      WHERE ${
 							queryString ? queryString : 1
 						} AND YEAR(i.inspection_date) = YEAR(NOW()) AND i.centre_id = ? `,
-		[centreId, firstDayOfMonth],
+		[req.user.centre_id, firstDayOfMonth],
 		(err, result, fields) => {
 			if (err) {
 				return res.status(500).json({
@@ -112,12 +108,11 @@ exports.countInspectionsOfEachCentreThisYear = async (req, res) => {
 // DONE nhưng không liên quan
 // đếm tổng số lượng đăng kiểm đã hết hạn của trung tâm mà staff đang làm việc
 exports.countTotalExpiredInspectionsOfEachCentre = async (req, res) => {
-	const centreId = req.params.centreId;
 	connection.query(
 		`SELECT COUNT(*) 
                     FROM inspections 
                     WHERE expired_date < NOW() AND centre_id = ?`,
-		[centreId],
+		[req.user.centre_id],
 		(err, result, fields) => {
 			if (err) {
 				return res.status(500).json({
@@ -142,7 +137,6 @@ exports.countTotalExpiredInspectionsOfEachCentreThisMonth = async (
 	req,
 	res
 ) => {
-	const centreId = req.params.centreId;
 	// Lấy ngày đầu tiên của tháng hiện tại
 	const firstDayOfMonth = new Date(
 		new Date().getFullYear(),
@@ -153,7 +147,7 @@ exports.countTotalExpiredInspectionsOfEachCentreThisMonth = async (
 		`SELECT COUNT(*) 
                     FROM inspections 
                     WHERE expired_date < NOW() AND centre_id = ? AND expired_date >= ?`,
-		[centreId, firstDayOfMonth],
+		[req.user.centre_id, firstDayOfMonth],
 		(err, result, fields) => {
 			if (err) {
 				return res.status(500).json({
@@ -175,13 +169,12 @@ exports.countTotalExpiredInspectionsOfEachCentreThisMonth = async (
 // DONE
 // đếm tổng số lượng đăng kiểm sắp hết hạn của các trung tâm staff đang làm việc (hiện tại <= ngày hết hạn <= hiện tại + 1 tháng)
 exports.countTotalAboutToExpiredInspectionsOfEachCentre = async (req, res) => {
-	const centreId = req.params.centreId;
 	connection.query(
 		`SELECT COUNT(*) 
                     FROM inspections 
                     WHERE expired_date >= NOW() 
                             AND expired_date <= DATE_ADD(NOW(), INTERVAL 1 MONTH) AND centre_id = ?`,
-		[centreId],
+		[req.user.centre_id],
 		(err, result, fields) => {
 			if (err) {
 				return res.status(500).json({
@@ -199,18 +192,18 @@ exports.countTotalAboutToExpiredInspectionsOfEachCentre = async (req, res) => {
 		}
 	);
 };
+// DONE
 // đếm tổng số lượng đăng kiểm sắp hết hạn của các trung tâm staff đang làm việc trong tháng này
 exports.countTotalAboutToExpiredInspectionsOfEachCentreThisMonth = async (
 	req,
 	res
 ) => {
-	const centreId = req.params.centreId;
 	connection.query(
 		`SELECT COUNT(*) 
                     FROM inspections 
                     WHERE expired_date >= NOW() 
                             AND expired_date <= LAST_DAY(NOW()) AND centre_id = ?`,
-		[centreId],
+		[req.user.centre_id],
 		(err, result, fields) => {
 			if (err) {
 				return res.status(500).json({
@@ -228,17 +221,16 @@ exports.countTotalAboutToExpiredInspectionsOfEachCentreThisMonth = async (
 		}
 	);
 };
-// NOT DONE
+// DONE
 // dự đoán số lượng xe đăng kiểm mới của mỗi trung tâm
 // (xe đăng ký dự đoán ở tỉnh nào thì dự đoán số lượng xe đăng kiểm mới của trung tâm ở tỉnh đó)
 exports.predictAboutToInspectOfEachCentre = async (req, res) => {
-	const centreId = req.params.centreId;
 	connection.query(
 		`SELECT COUNT(*) 
                     FROM cars c INNER JOIN registration_centres r
                     ON c.predictedAddress = r.address 
                     WHERE c.inspected = false AND r.centre_id = ?`,
-		[centreId],
+		[req.user.centre_id],
 		(err, result, fields) => {
 			if (err) {
 				return res.status(500).json({
@@ -258,12 +250,11 @@ exports.predictAboutToInspectOfEachCentre = async (req, res) => {
 // DONE
 // đưa ra thông tin đăng kiểm của trung tâm mà staff đang làm việc
 exports.getInspection = (req, res) => {
-	const centreId = req.params.centreId;
 	let queryString = utils.generateQueryString(req.query);
 	connection.query(
 		`SELECT * FROM inspections 
                     WHERE ${queryString ? queryString : 1} AND centre_id = ?`,
-		[centreId],
+		[req.user.centre_id],
 		(err, result, fields) => {
 			if (err) {
 				return res.status(500).json({
@@ -291,7 +282,6 @@ exports.getInspection = (req, res) => {
 // đưa ra thông tin đăng kiểm của trung tâm mà staff đang làm việc bao gồm thông tin chi tiết
 // tất cả thông tin chi tiết ( nối ba bảng gồm: inspections, cars, car_owners)
 exports.getInspectionAndOwner = (req, res) => {
-	const centreId = req.params.centreId;
 	let queryString = utils.generateQueryString(req.query);
 	connection.query(
 		`SELECT * 
@@ -299,7 +289,7 @@ exports.getInspectionAndOwner = (req, res) => {
                     INNER JOIN cars c ON c.car_id = i.car_id
                     INNER JOIN car_owners co ON co.owner_id = c.owner_id
                     WHERE ${queryString ? queryString : 1} AND i.centre_id = ?`,
-		[centreId],
+		[req.user.centre_id],
 		(err, result, fields) => {
 			if (err) {
 				return res.status(500).json({
@@ -322,94 +312,130 @@ exports.getInspectionAndOwner = (req, res) => {
 		}
 	);
 };
-
-// Tạo đăng kiểm
-// đang để tạm user_id = 1, centre_id = 1, specify = "carry_people$lte:9-personal+manufacture$lte:7~36~24", first_time = 0
+// NOT DONE
+// Tạo đăng kiểm cho trung tâm mà staff đang làm việc
+// đang để tạm  specify = "carry_people$lte:9-personal+manufacture$lte:7~36~24"
 // vì chưa có chức năng đăng nhập
 exports.createInspection = (req, res) => {
-	const number_plate = req.body.number_plate; // Thay vì sử dụng req.params, đọc từ req.body
+	const number_plate = req.body.number_plate;
+	const wheel_formula = req.body.wheel_formula; // công thức bánh xe
+	const wheel_tread = req.body.wheel_tread; // vết bánh xe
+	const overall_dimension = req.body.overall_dimension; // kích thước bao
+	const container_dimension = req.body.container_dimension; // kích thước lòng thùng xe
+	const length_base = req.body.length_base; // chiều dài cơ sở
+	const kerb_mass = req.body.kerb_mass; // khối lượng bản thân
+	const designed_and_authorized_payload =
+		req.body.designed_and_authorized_payload; // Khối lượng hàng CC theo TK/CP TGGT
+	const designed_and_authorized_total_mass =
+		req.body.designed_and_authorized_total_mass; // Khối lượng toàn bộ theo TK/CP TGGT
+	const designed_and_authorized_towed_mass =
+		req.body.designed_and_authorized_towed_mass; // Khối lượng kéo theo TK/CP TGGT
+	const permissible_carry = req.body.permissible_carry; // Số lượng người cho phép chở
+	const engine_displacement = req.body.engine_displacement; // Thể tích làm việc của động cơ
+	const maximum_output_to_rpm_ratio = req.body.maximum_output_to_rpm_ratio; // Công suất lớn nhất/tốc độ quay
+	const fuel = req.body.fuel; // Loại nhiên liệu
+	const number_of_tires = req.body.number_of_tires; // Số lượng lốp
+	const tire_size = req.body.tire_size; // Cỡ lốp/trục
 	let queryString = utils.generateQueryString(req.query);
 
+	const inspection_date = new Date();
+	const expired_date = new Date(inspection_date);
+	expired_date.setMonth(expired_date.getMonth() + 6);
+
+	// Truy vấn con để đếm số lần đăng kiểm
 	connection.query(
-		`SELECT *
-        FROM inspections i
-        INNER JOIN cars c ON c.car_id = i.car_id WHERE i.car_id = (SELECT car_id FROM cars WHERE number_plate = ?)`,
+		`SELECT COUNT(*) as count FROM inspections WHERE car_id = (SELECT car_id FROM cars WHERE number_plate = ?)`,
 		[number_plate],
-		(err, result, fields) => {
-			if (err) {
+		(countErr, countResult, countFields) => {
+			if (countErr) {
 				return res.status(500).json({
 					status: "Failed",
-					message: err,
+					message: countErr,
 				});
-			} else {
-				const inspection_date = new Date();
-				const expired_date = new Date(inspection_date);
-				expired_date.setMonth(expired_date.getMonth() + 6);
-				connection.query(
-					`INSERT INTO inspections (inspection_id, inspection_number, inspection_date, car_id, user_id, centre_id, specify, first_time, expired_date)
-						SELECT
-    						COALESCE(MAX(inspection_id), 0) + 1,
-							CONCAT(YEAR(CURDATE()), '-', LPAD(COALESCE(MAX(CAST(SUBSTRING(inspection_number, 6) AS UNSIGNED)), 0) + 1, 6, '0')),
-							?,
-							(SELECT car_id FROM cars WHERE number_plate = ?),
-							?, ?, ?, ?,
-							?
-						FROM inspections;`,
-					[
-						inspection_date,
-						number_plate,
-						"1",
-						"1",
-						"carry_people$lte:9-personal+manufacture$lte:7~36~24",
-						"0",
-						expired_date,
-					],
-					(insertErr, insertResult, insertFields) => {
-						if (insertErr) {
-							return res.status(500).json({
-								status: "Failed",
-								message: insertErr,
-							});
-						} else {
+			}
+
+			const first_time = countResult[0].count === 0 ? 1 : 0;
+
+			// Câu lệnh INSERT sử dụng giá trị của first_time được xác định ở trên
+			connection.query(
+				`INSERT INTO inspections (inspection_id, inspection_number, inspection_date, car_id, user_id, centre_id, specify, first_time, expired_date)
+          SELECT
+            COALESCE(MAX(inspection_id), 0) + 1,
+            CONCAT(YEAR(CURDATE()), '-', LPAD(COALESCE(MAX(CAST(SUBSTRING(inspection_number, 6) AS UNSIGNED)), 0) + 1, 6, '0')),
+            ?,
+            (SELECT car_id FROM cars WHERE number_plate = ?),
+            ?, ?, ?, ?,
+            ?
+          FROM inspections`,
+				[
+					inspection_date,
+					number_plate,
+					req.user.user_id,
+					req.user.centre_id,
+					"carry_people$lte:9-personal+manufacture$lte:7~36~24",
+					first_time,
+					expired_date,
+				],
+				(insertErr, insertResult, insertFields) => {
+					if (insertErr) {
+						return res.status(500).json({
+							status: "Failed",
+							message: insertErr,
+						});
+					}
+
+					// Câu lệnh UPDATE
+					connection.query(
+						`UPDATE cars SET inspected = 1,
+						wheel_formula = ?,
+						wheel_tread = ?,
+					    overall_dimension = ?,
+						container_dimension = ?,
+						length_base = ?,
+						kerb_mass = ?,
+						designed_and_authorized_payload = ?,
+						designed_and_authorized_total_mass = ?,
+						designed_and_authorized_towed_mass = ?, 
+						permissible_carry = ?,
+						engine_displacement = ?,
+						maximum_output_to_rpm_ratio = ?,
+						fuel = ?,
+						number_of_tires_and_tire_size=?
+							WHERE number_plate = ?`,
+						[
+							wheel_formula,
+							wheel_tread + " (mm)",
+							overall_dimension + " (mm)",
+							container_dimension + " (mm)",
+							length_base + " (mm)",
+							kerb_mass + " (kg)",
+							designed_and_authorized_payload + " (kg)",
+							designed_and_authorized_total_mass + " (kg)",
+							designed_and_authorized_towed_mass + " (kg)",
+							permissible_carry,
+							engine_displacement + " (cm3)",
+							maximum_output_to_rpm_ratio,
+							fuel,
+							number_of_tires + " tires, " + tire_size,
+							number_plate,
+						],
+						(updateErr, updateResult, updateFields) => {
+							if (updateErr) {
+								return res.status(500).json({
+									status: "Failed",
+									message: updateErr,
+								});
+							}
+
 							return res.status(201).json({
 								status: "Success",
 								message: "Create inspection successfully",
 								data: insertResult,
 							});
 						}
-					}
-				);
-			}
+					);
+				}
+			);
 		}
 	);
-};
-exports.getInspectionX = (req, res) => {
-	login(req, res, () => {
-		console.log(centre_id);
-		connection.query(
-			`SELECT * FROM users WHERE centre_id=?`,
-			[centre_id],
-
-			(err, result, fields) => {
-				if (err) {
-					return res.status(500).json({
-						status: "Failed",
-						message: err,
-					});
-				} else if (result.length == 0) {
-					return res.status(404).json({
-						status: "Failed",
-						message: `Can't find inspection with ${utils.generateErrorQueryValue(
-							req.query
-						)}`,
-					});
-				} else {
-					return res.status(200).json({
-						status: "Success",
-						data: result,
-					});
-				}
-			}
-		);
-	});
 };
--- server/routes/inspectionRoute.js
+++ server/routes/inspectionRoute.js
@@ -1,55 +1,78 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const inspectionController = require("../controllers/inspectionController");
+const authController = require("../controllers/authController");
 // đếm tổng số lượng đăng kiểm của centre mà staff đang làm việc
 router.get(
-	"/stat/each_centre/count/:centreId",
+	"/stat/each_centre/count/",
+	authController.authenticateToken,
 	inspectionController.countInspectionsOfEachCentre
 );
 // đếm tổng số lượng đăng kiểm của centre mà staff đang làm việc trong tháng này
 router.get(
-	"/stat/each_centre/count/month/:centreId",
+	"/stat/each_centre/count/month/",
+	authController.authenticateToken,
 	inspectionController.countInspectionsOfEachCentreThisMonth
 );
 // đếm tổng số lượng đăng kiểm của centre mà staff đang làm việc trong năm này
 router.get(
-	"/stat/each_centre/count/year/:centreId",
+	"/stat/each_centre/count/year/",
+	authController.authenticateToken,
 	inspectionController.countInspectionsOfEachCentreThisYear
 );
 // DONE nhưng không liên quan
 // đếm tổng số lượng đăng kiểm đã hết hạn của trung tâm mà staff đang làm việc
 router.get(
-	"/stat/each_centre/count/expired/:centreId",
+	"/stat/each_centre/count/expired/",
+	authController.authenticateToken,
 	inspectionController.countTotalExpiredInspectionsOfEachCentre
 );
 // DONE
 // đếm tổng số lượng đăng kiểm đã hết hạn của trung tâm mà staff đang làm việc trong tháng này
 router.get(
-	"/stat/each_centre/count/expired/month/:centreId",
+	"/stat/each_centre/count/expired/month/",
+	authController.authenticateToken,
 	inspectionController.countTotalExpiredInspectionsOfEachCentreThisMonth
 );
 // DONE
 // đếm tổng số lượng đăng kiểm sắp hết hạn của các trung tâm staff đang làm việc (hiện tại <= ngày hết hạn <= hiện tại + 1 tháng)
 router.get(
-	"/stat/each_centre/count/about_to_expired/:centreId",
+	"/stat/each_centre/count/about_to_expired/",
+	authController.authenticateToken,
 	inspectionController.countTotalAboutToExpiredInspectionsOfEachCentre
 );
 // đếm tổng số lượng đăng kiểm sắp hết hạn của các trung tâm staff đang làm việc trong tháng này
 router.get(
-	"/stat/each_centre/count/about_to_expired/month/:centreId",
+	"/stat/each_centre/count/about_to_expired/month/",
+	authController.authenticateToken,
 	inspectionController.countTotalAboutToExpiredInspectionsOfEachCentreThisMonth
 );
+// dự đoán số lượng xe đăng kiểm mới của mỗi trung tâm
+// (xe đăng ký dự đoán ở tỉnh nào thì dự đoán số lượng xe đăng kiểm mới của trung tâm ở tỉnh đó)
 router.get(
-	"/stat/all_centre/prediction/about_to_inspect_of_each_centre/:centreId",
+	"/stat/all_centre/prediction/about_to_inspect_of_each_centre/",
+	authController.authenticateToken,
 	inspectionController.predictAboutToInspectOfEachCentre
 );
-router.get("/x/", inspectionController.getInspectionX);
-router.get("/:centreId", inspectionController.getInspection);
 
+// đưa ra thông tin đăng kiểm của trung tâm mà staff đang làm việc
 router.get(
-	"/get_inspection_owner/:centreId",
+	"/",
+	authController.authenticateToken,
+	inspectionController.getInspection
+);
+// đưa ra thông tin đăng kiểm của trung tâm mà staff đang làm việc bao gồm thông tin chi tiết
+// tất cả thông tin chi tiết ( nối ba bảng gồm: inspections, cars, car_owners)
+router.get(
+	"/get_inspection_owner/",
+	authController.authenticateToken,
 	inspectionController.getInspectionAndOwner
 );
-router.post("/createInspection/", inspectionController.createInspection);
+// Tạo đăng kiểm cho trung tâm mà staff đang làm việc
+router.post(
+	"/createInspection/",
+	authController.authenticateToken,
+	inspectionController.createInspection
+);
 
 module.exports = router;
