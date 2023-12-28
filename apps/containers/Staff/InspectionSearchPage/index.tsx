d391b36049edc1bfb163fd2ad5141b110be24fbb
Nguyen Dang Quang <quangnguyenvk@gmail.com>
2023-12-28 21:34:11 +0700

    Add breadcrumb, few fix UI table

--- apps/containers/Staff/InspectionSearchPage/index.tsx
+++ apps/containers/Staff/InspectionSearchPage/index.tsx
@@ -3,7 +3,7 @@ import styles from "./index.module.scss";
 import Flex from "@/modules/ui/components/Flex";
 import NavBar from "../NavBar";
 import SearchForm from "./containers/SearchForm";
-
+import { Breadcrumb } from "antd";
 export type Props = {
     className?: string;
     style?: React.CSSProperties;
@@ -20,10 +20,30 @@ export default function InspectionSearchPage({ className, style }: Props) {
                         display: "flex",
                         flexDirection: "column",
                         background: "#e4e4e7",
-                        alignItems: "center",
                     }}
                 >
-                    <div style={{ width: "60%", margin: 50 }}>
+                    <Breadcrumb
+                        separator=">"
+                        items={[
+                            {
+                                title: "Nhà của tôi",
+                            },
+                            {
+                                title: "Quản lý đăng kiểm",
+                            },
+                            {
+                                title: "Tra cứu đăng kiểm",
+                            },
+                        ]}
+                        style={{ marginLeft: 30, marginTop: 30 }}
+                    />
+                    <div
+                        style={{
+                            width: "60%",
+                            margin: 50,
+                            alignSelf: "center",
+                        }}
+                    >
                         <SearchForm />
                     </div>
                 </div>
