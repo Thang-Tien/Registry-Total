f2f0de6054f62f62f10e3701ebe8eb11eeb907cb
thanhthien1305 <100114914+thanhthien1305@users.noreply.github.com>
2023-12-29 20:16:31 +0700

    hasagi

--- apps/containers/Admin/Authentication/module/Login.tsx
+++ apps/containers/Admin/Authentication/module/Login.tsx
@@ -35,7 +35,7 @@ const LoginForm = (props: LoginFormProps) => {
 
     try {
       const response = await fetch(
-        `/api/v1/users/login`,
+        `http://localhost:8000/api/v1/users/login`,
         {
           method: "POST",
           headers: {
