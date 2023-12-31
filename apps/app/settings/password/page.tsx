9dcd1591fece9a9cf545aedbb1a249e67b685df6
thanhthien1305 <100114914+thanhthien1305@users.noreply.github.com>
2024-01-01 03:51:27 +0700

    done setting

---
+++ apps/app/settings/password/page.tsx
@@ -0,0 +1,8 @@
+
+import React from "react";
+import SettingsPage from "../../../containers/Staff/SettingsPage";
+
+ function RouteToPassword() {
+    return <SettingsPage mode ="password"/>
+}
+export default RouteToPassword
---
+++ apps/containers/Staff/SettingsPage/index.tsx
@@ -0,0 +1,55 @@
+"use client"
+import Settings from "./module/Settings";
+import cx from "classnames";
+import styles from "./styles/index.module.scss";
+import Flex from "@/modules/ui/components/Flex";
+import NavBar from "../NavBar";
+
+import { Breadcrumb } from "antd";
+
+
+export type Props = {
+  mode: string,
+    className?: string;
+    style?: React.CSSProperties;
+};
+
+export default function SettingsPage({ className, style, mode}: Props) {
+    return (
+        <div className={cx(className, styles.container)} style={style}>
+            <Flex.Row style={{ width: "100%", minHeight: "100vh" }}>
+                <NavBar active="settings" />
+                <div
+                    style={{
+                        flex: "1",
+                        display: "flex",
+                        flexDirection: "column",
+                        background: "#e4e4e7",
+                    }}
+                >
+                    <Breadcrumb
+                        separator=">"
+                        items={[
+                            {
+                                title: "Nhà của tôi",
+                            },
+                            {
+                                title: "Cài đặt",
+                            },
+                        ]}
+                        style={{ marginLeft: 30, marginTop: 30 }}
+                    />
+                    <div
+                        style={{
+                            width: "90%",
+                            marginTop: 30,
+                            alignSelf: "center",
+                        }}
+                    >
+                        <Settings mode={mode} />
+                    </div>
+                </div>
+            </Flex.Row>
+        </div>
+    );
+}
