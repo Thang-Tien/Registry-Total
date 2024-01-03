22230fc9bb7f528743924b01f062c799efe40852
thanhthien1305 <100114914+thanhthien1305@users.noreply.github.com>
2024-01-03 21:52:59 +0700

    change name file

--- apps/app/inspections/[id]/page.tsx
+++ apps/app/inspections/[id]/page.tsx
@@ -1,89 +1,26 @@
-"use client";
+// pages/inspections/[...id]/page.tsx
+import { usePathname } from "next/navigation";
+import React from "react";
+import InspectionInformationPage from "../../../containers/Staff/InspectionInformationPage";
 
-import { Button, Card } from "antd";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { IoArrowBackOutline } from "react-icons/io5";
-import InspectionProfile from "../../../containers/Admin/Inspection/InspectionProfile";
-import Flex from "@/modules/ui/components/Flex";
-import NavBar from "../../../containers/Admin/NavBar";
+export async function generateStaticParams() {
+    const totalInspections = 10000; // Adjust based on your actual car count
+    const dynamicPaths = Array.from(
+        { length: totalInspections },
+        (_, index) => ({
+            id: `${index + 1}`,
+        })
+    );
 
-export default function RouteToInspectionProfile({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const [data, setData] = useState({ car_id: 1, user_id: 1, centre_id: 1 });
-  const [loading, setLoading] = useState(false);
-  const router = useRouter();
+    return dynamicPaths;
+}
 
-  useEffect(() => {
-    setLoading(true);
+export default function RouteToInspectionInformationPage(
+    { params } = {} as any
+) {
+    const id = params.id;
 
-    const getData = async () => {
-      try {
-        let response = await fetch(
-          `http://fall2324w3g10.int3306.freeddns.org/api/v1/inspections?inspection_id=${Number(
-            params.id
-          )}`
-        );
-        if (!response.ok) throw new Error("Fail to get data");
+    console.log(id);
 
-        let tmp = await response.json();
-        setData(tmp.data[0]);
-        setLoading(false);
-      } catch (error) {
-        setLoading(false);
-        console.log(error);
-      }
-    };
-    getData();
-  }, []);
-
-  return (
-    <div>
-      <Flex.Row style={{ height: "100vh", width: "100vw" }} padding={"0"}>
-        <NavBar active="inspections" openMenu="" />"
-        <div
-          style={{
-            background: "#e4e4e7",
-            width: "calc(100vw - 256px - 8px)",
-            overflow: "auto",
-            display: "flex",
-            justifyContent: "center",
-            padding: "50px 0",
-          }}
-        >
-          <Flex.Cell style={{ marginBottom: "500px", maxWidth:"800px" }}>
-            <Card
-              loading={loading}
-              title={
-                <Button
-                  type="text"
-                  icon={
-                    <IoArrowBackOutline
-                      style={{ fontSize: "2rem", verticalAlign: "middle" }}
-                    />
-                  }
-                  onClick={() => {
-                    router.back();
-                  }}
-                />
-              }
-            >
-              <Card.Meta
-                style={{ padding: "0 3rem " }}
-                title={
-                  <div style={{ marginBottom: "3rem" }}>
-                    Thông tin đăng kiểm
-                  </div>
-                }
-                description={<InspectionProfile data={data} />}
-              />
-            </Card>
-          </Flex.Cell>
-        </div>
-      </Flex.Row>
-    </div>
-  );
+    return <InspectionInformationPage inspectionId={id} />;
 }
