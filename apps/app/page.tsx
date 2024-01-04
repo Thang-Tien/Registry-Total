4fb7a23b59962ef52d4138c18177ac7bcadcf9c2
thanhthien1305 <100114914+thanhthien1305@users.noreply.github.com>
2024-01-04 20:46:44 +0700

    done

--- apps/app/page.tsx
+++ apps/app/page.tsx
@@ -1,15 +1,26 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
+
 export default function RouteToHomePage() {
-  const router = useRouter();
+    const router = useRouter();
+    const [me, setMe] = useState({
+      name: '', address: '', phone: 0, email: '', date_of_birth: '', role: '', ssn: '', centre_id: 0,
+    });
+    const df = { name: "Thiện pờ rồ", age: 18 };
+    useEffect(()=>{
+      if(me.role ==="staff") router.push("/dashboards") 
+      if(me.role === "admin") router.push("/dashboard");
+    },[me]);
     useEffect(() => {
         if(localStorage.length === 0)
         router.push("/auth"); 
-      else 
-      router.push("/dashboards"); 
+      else {
+        const data = localStorage.getItem("data") === null ? JSON.stringify(df) : localStorage.getItem("data");
+        if (data != null) setMe(JSON.parse(data));
+      }
     }, []);
 
   return <div></div>;
